d610e59a583402b05d2bd1504f1b31a4a11b300d
refikbasic <120068378+refikbasic@users.noreply.github.com>
2023-01-29 21:18:57 +0100

    Add files via upload

---
+++ age_verification.py
@@ -0,0 +1,7 @@
+godine=int(input("Please input your age: "))
+if godine <1:
+    print("Please enter positive number.")
+elif godine <18:
+    print("You are not 18, forbidden entry!")
+elif godine >=18:
+    print("Welcome!")
